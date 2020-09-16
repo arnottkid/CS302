9fe4a3929285d679f0e46207082dcacd0c803ef4
Jim Plank <plank@cs.utk.edu>
2020-09-16 00:44:04 -0400

    Sudoku.

--- Recursion-Review/src/sudoku2.cpp
+++ Recursion-Review/src/sudoku2.cpp
@@ -12,7 +12,7 @@ class Sudoku {
     void Print() const;                 // Print to standard output
     int row_ok(int r) const;            // Test row r for correctness
     int column_ok(int c) const;         // Test cols r for correctness
-    int panel_ok(int pr, int pc) const; // Test panel pr/pc (both 0,1,2) for correctness
+    int panel_ok(int pr, int pc) const; // Test panel pr/pc (both 0,1,2)
   protected:
     vector <string> puzzle;             // Hold the puzzle in a vector of 9 strings
 };
